3ec0fadfbf773bea09f11f4679744beb49ccb2f2
Arès Méroueh <ares.meroueh@gmail.com>
2016-09-11 21:12:19 +0100

    Commented card.h and removed the clean-up script

--- source/global/card.h
+++ source/global/card.h
@@ -7,9 +7,23 @@
 #include <cstdint>
 #include <cstdio>
 
-typedef std::int8_t Card;
+/*A card an 8 bit integer; the first four bits represent the height; the last four the suit */
+typedef std::int8_t Card; 
 typedef std::int8_t Height;
 typedef std::int8_t Suit;
+
+/*strToCard converts a string to a card
+	example: strToCard("Kh"); gives the king of hearts;
+	
+ heightToChar converts a height to a char
+ 	example: heightToChar(14); gives 'A' for ace
+ 
+ parseCards reads a string of the form [Kh,As,5c] and stores the corresponding 		result in an array of cards. There should be between 1 and 5 cards. 
+ 	example: 
+ 	Card cards[3]; 
+ 	parseCards("[Kh,As,5c]", cards);
+ 
+getHeight and getSuit give the height and suit of a card, respectively.*/
 
 Card strToCard(char*);
 char heightToChar(Height);
