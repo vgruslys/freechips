690589d5b06bde81b9b46c8539867ec2fec1ef61
Arès Méroueh <ares.meroueh@gmail.com>
2016-09-12 14:09:28 +0100

    Correct random_deck.h

--- source/random/random_deck.h
+++ source/random/random_deck.h
@@ -2,9 +2,9 @@
 class RandomDeck
 {
     public:
-        void removeCard(Card) = 0; //Removes a card from the deck
-        void addCard(Card) = 0; //Adds a card to the deck
-        void reset(); //Adds all the missing cards back to the deck
-        Card drawCard() = 0; //Draws a card from the deck
-        void drawCard(Card*, int) = 0; //Draws n cards from the deck and stores then in an array of cards
+        virtual void removeCard(Card) = 0; //Removes a card from the deck
+        virtual void addCard(Card) = 0; //Adds a card to the deck
+        virtual void reset() = 0; //Adds all the missing cards back to the deck
+        virtual Card drawCard() = 0; //Draws a card from the deck
+        virtual void drawCard(Card*, int) = 0; //Draws n cards from the deck and stores them in an array of cards
 };
