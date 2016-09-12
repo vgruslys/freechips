323f02c6aa4f0e7b7734733f4e9bef831cec4536
Arès Méroueh <ares.meroueh@gmail.com>
2016-09-12 14:18:13 +0100

    More corrections

--- source/random/randomizer.h
+++ source/random/randomizer.h
@@ -1,7 +1,10 @@
+#ifndef __RANDOMIZER__
+#define __RANDOMIZER__
+
 class Randomizer
 {
     public:
-        virtual int randInt(int, int) = 0;
-        virtual int randWeights(int*, int) = 0;
-        
+        virtual int randInt(int, int) = 0; // draw an int in range [a,b]
+        virtual int randWeights(int*, int) = 0; //returns an index based on weights found in array
 };
+#endif
