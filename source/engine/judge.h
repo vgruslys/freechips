301bba386a7d2dcb791106e5dfb69d033001eb3c
Arès Méroueh <ares.meroueh@gmail.com>
2016-09-15 22:29:25 +0100

    Change Evaluator to Judge

--- source/engine/judge.h
+++ source/engine/judge.h
@@ -1,9 +1,9 @@
-#ifndef __EVALUATOR__
-#define __EVALUATOR__
+#ifndef __JUDGE__
+#define __JUDGE__
 #include "community.h"
 #include "player_cards.h"
 /*Evaluator - a class which determines the score of a community + player hand */
-class Evaluator {
+class Judge {
 public:
 	virtual int evaluate(const Community&, const PlayerCards&);
 	virtual int evaluate(const Community&, const PlayerCards&, const PlayerCards&);
