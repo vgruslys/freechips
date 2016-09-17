479d0a727f0234a6c6e955ae76b74a315df925f1
Arès Méroueh <ares.meroueh@gmail.com>
2016-09-17 10:17:48 +0100

    Clean up JudgeTable :: verdict

--- source/engine/judge_table.cpp
+++ source/engine/judge_table.cpp
@@ -6,42 +6,10 @@
 using namespace std;
 int JudgeTable::verdict(const Community& com, const PlayerCards& pc) const {
 	
-	/*static internal variables */
-	static int _flush_score =0; //stores the flush score
-	static  int _unsuited_score =0; //stores the unsuited score
-	
-	//keys
-	static uint64_t _key= 0; //the normal 64bit community key
-	static uint64_t _coded_key = 0; //the coded community key 
-	static uint64_t _suit_key = 0; //the small suit key extrated from the coded community key
-	static uint64_t _unsuited_key = 0; //the unsuited key extracted from the coded community key 
-	static uint64_t _suit_shift = 0; // the bit shift deduced from the _suit_key
-	static uint64_t _player_key = 0; // the normal 64bit player key
-	static uint64_t _player_unsuited_key = 0; // the player unsuited key (should be a small int representing the height of the two cards that he has)
-	
-	//masks
-	static uint64_t _mask13 = 0x0000000000001fff;
-	static uint64_t _mask32 = 0x00000000ffffffff;
-	
-	/*Load community keys */
-	_key = com.getKey();
-	_coded_key = com.getCodedKey();
-	
-	/*extract the two coded keys */
-	_unsuited_key = _coded_key & _mask32;
-	_suit_key = _coded_key >> 32;
-	
-	/*Load player keys*/
-	_player_key = pc.getKey();
-	_player_unsuited_key = pc.getCodedKey();
-	
-	/*Find the suit which could potentially give a flush, & the corresponding bit shift*/
-	_suit_shift = _suit_map[_suit_key];
-
 	/*Compute flush score */
-	_flush_score = _flush_table[((_key >> _suit_shift) & _mask13) | ((_player_key >> _suit_shift) & _mask13)];
+	int _flush_score = _flush_table[((com.getKey() >> _suit_map[com.getCodedKey()>>32]) & 0x0000000000001fff) | ((pc.getKey() >> _suit_map[com.getCodedKey()>>32]) & 0x0000000000001fff)];
 	/*Compute the unsuited score*/
-	_unsuited_score = _unsuited_table[_unsuited_key][_player_unsuited_key];
+	int _unsuited_score=_unsuited_table[com.getCodedKey() & 0x00000000ffffffff][pc.getCodedKey()];
 	
 	return _flush_score > _unsuited_score? _flush_score : _unsuited_score; // returns max of flush and unsuited score
 }
